5202af34ecb8b376a540a85d26837cfc1006bde3
Ekimtsov.av <temkaru84@gmail.com>
2023-09-24 23:02:27 +0700

    2.1 «DOM». Появление элементов при прокрутке.

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,3 +2,18 @@ const revealElements = document.querySelectorAll('.reveal');
 
 window.addEventListener('scroll', revealElementsOnScroll);
 
+function revealElementsOnScroll() {
+    const windowHeight = window.innerHeight;
+
+    revealElements.forEach((element) => {
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight) {
+            element.classList.add('reveal_active');
+        } else {
+            element.classList.remove('reveal_active');
+        }
+    });
+}
+
+revealElementsOnScroll();
