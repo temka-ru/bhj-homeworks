ca8205e6850ddb536b492e30992988d8d4aa64de
Ekimtsov.av <temkaru84@gmail.com>
2023-09-27 11:34:29 +0700

    Fix-2 2.3 «Изменение структуры HTML-документа». Всплывающая подсказка.

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,29 +2,31 @@ const tooltipElements = document.querySelectorAll('.has-tooltip');
 
 function displayTooltip(event) {
     event.preventDefault();
+    
     const tooltipText = this.getAttribute('title');
     const tooltip = document.createElement('div');
     tooltip.classList.add('tooltip');
     tooltip.textContent = tooltipText;
-
+   
     const rect = this.getBoundingClientRect();
     tooltip.style.left = rect.left + 'px';
-    tooltip.style.top = rect.bottom + 'px';
-
+    tooltip.style.top = rect.bottom + 'px';    
+    const hasTooltipElements = document.querySelectorAll('.has-tooltip');
     const activeTooltip = document.querySelector('.tooltip_active');
-    if (activeTooltip && activeTooltip !== tooltip) {
+    
+    if (activeTooltip && Array.from(hasTooltipElements).includes(activeTooltip.parentElement)) {
         activeTooltip.classList.remove('tooltip_active');
         activeTooltip.remove();
-    }
-    if (tooltip.classList.contains('tooltip_active')) {
-        tooltip.classList.remove('tooltip_active');
-        tooltip.remove();
     } else {
+        if (activeTooltip) {
+            activeTooltip.classList.remove('tooltip_active');
+            activeTooltip.remove();
+        }
         tooltip.classList.add('tooltip_active');
-        document.body.appendChild(tooltip);
+        this.appendChild(tooltip);
     }
   }
-  
+
 tooltipElements.forEach(element => {
     element.addEventListener('click', displayTooltip);
 });
