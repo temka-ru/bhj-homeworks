c031d7522d4397c40ee694a9c9fc0c65a5a8f348
Ekimtsov.av <temkaru84@gmail.com>
2023-09-27 19:28:26 +0700

    2.3 «Изменение структуры HTML-документа». Всплывающая подсказка.

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,32 +1,32 @@
-const tooltipElements = document.querySelectorAll('.has-tooltip');
-
-function displayTooltip(event) {
-    event.preventDefault();
-    
-    const tooltipText = this.getAttribute('title');
-    const tooltip = document.createElement('div');
-    tooltip.classList.add('tooltip');
-    tooltip.textContent = tooltipText;
-   
-    const rect = this.getBoundingClientRect();
-    tooltip.style.left = rect.left + 'px';
-    tooltip.style.top = rect.bottom + 'px';    
-    const hasTooltipElements = document.querySelectorAll('.has-tooltip');
-    const activeTooltip = document.querySelector('.tooltip_active');
-    
-    if (activeTooltip && Array.from(hasTooltipElements).includes(activeTooltip.parentElement)) {
-        activeTooltip.classList.remove('tooltip_active');
-        activeTooltip.remove();
-    } else {
-        if (activeTooltip) {
-            activeTooltip.classList.remove('tooltip_active');
-            activeTooltip.remove();
-        }
-        tooltip.classList.add('tooltip_active');
-        this.appendChild(tooltip);
-    }
-  }
-
-tooltipElements.forEach(element => {
-    element.addEventListener('click', displayTooltip);
+const tooltipElements = document.querySelectorAll('.has-tooltip'); 
+let activeTooltip = null; 
+ 
+function displayTooltip(event) { 
+    event.preventDefault(); 
+    const tooltipText = this.getAttribute('title'); 
+    const tooltip = document.createElement('div'); 
+    tooltip.classList.add('tooltip'); 
+    tooltip.textContent = tooltipText; 
+    const rect = this.getBoundingClientRect(); 
+    tooltip.style.left = rect.left + 'px'; 
+    tooltip.style.top = rect.bottom + 'px'; 
+     
+    if (activeTooltip) { 
+        if (activeTooltip.textContent === tooltipText) { 
+            activeTooltip.classList.toggle('tooltip_active'); 
+            return; 
+        } else { 
+            activeTooltip.classList.remove('tooltip_active'); 
+            activeTooltip.remove(); 
+            activeTooltip = null; 
+        } 
+    } 
+     
+    tooltip.classList.add('tooltip_active'); 
+    document.body.appendChild(tooltip); 
+    activeTooltip = tooltip; 
+} 
+ 
+tooltipElements.forEach(element => { 
+    element.addEventListener('click', displayTooltip); 
 });
